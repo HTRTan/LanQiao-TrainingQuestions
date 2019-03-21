698064e950862ad912e980f027767166621b8287
HTRTan <tan@htrtan>
2019-03-21 15:24:21 +0800

    回溯算法.八皇后问题

--- src/回溯算法/八皇后问题/Main.java
+++ src/回溯算法/八皇后问题/Main.java
@@ -7,15 +7,15 @@ public class Main {
 	public static void main(String[] args) {
 		//初始化
 		
-		Taken = new boolean[8][8];
-		for(int x = 0 ; x < 8 ; x++){
-			for(int y = 0 ; y < 8 ; y++){
+		Taken = new boolean[20][20];
+		for(int x = 0 ; x < Taken.length ; x++){
+			for(int y = 0 ; y < Taken[x].length ; y++){
 				Taken[x][y]=false;
 			}
 		}
 		eightQueens(Taken,0);
-		for(int x = 0 ; x < 8 ; x++){
-			for(int y = 0 ; y < 8 ; y++){
+		for(int x = 0 ; x < Taken.length ; x++){
+			for(int y = 0 ; y < Taken[x].length ; y++){
 				System.out.print(Taken[x][y]?"○":"□");
 			}
 			System.out.println("");
@@ -26,12 +26,12 @@ public class Main {
 		if(!isDontPut(ThisTaken)){
 			return false;
 		}
-		if(num_queens==8){
+		if(num_queens==ThisTaken.length){
 			return true;
 		}
 		
-		for(int x = 0 ; x < 8 ; x++){
-			for(int y = 0 ; y < 8 ; y++){
+		for(int x = 0 ; x < ThisTaken.length ; x++){
+			for(int y = 0 ; y < ThisTaken[x].length ; y++){
 				//检查这个位置是否已被占用
 				if(ThisTaken[x][y]==false){
 					ThisTaken[x][y]=true;
@@ -49,11 +49,11 @@ public class Main {
 	
 	//考察这个方案是否合理
 	public static boolean isDontPut(boolean[][] ThisToken){
-		for(int temx = 0 ; temx < 8 ; temx++){
-			for(int temy = 0 ; temy < 8 ; temy++){
+		for(int temx = 0 ; temx < ThisToken.length ; temx++){
+			for(int temy = 0 ; temy < ThisToken[temx].length  ; temy++){
 				if(ThisToken[temx][temy]){
-					for(int x = 0 ; x < 8 ; x++){
-						for(int y = 0 ; y < 8 ; y++){
+					for(int x = 0 ; x < ThisToken.length ; x++){
+						for(int y = 0 ; y < ThisToken[x].length ; y++){
 							if(ThisToken[temx][y] && temy!=y)return false;
 							if(ThisToken[x][temy] && temx!=x)return false;
 							if(temx-x==temy-y && ThisToken[x][y] && temx!=x && temy!=y)return false;
